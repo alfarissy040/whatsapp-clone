79fb96e9c8feb53e8ca66c8af8abb4c1f5eea846
alfarissy040 <alfarissy040@gmail.com>
2022-06-16 21:19:24 +0700

    menupdate livewire.card dan app.index

--- app/Http/Livewire/Card.php
+++ app/Http/Livewire/Card.php
@@ -6,8 +6,36 @@ use Livewire\Component;
 
 class Card extends Component
 {
+    public $model = [
+        'img' => '',
+        'title' => '',
+        'excerpt' => '',
+        'time' => '',
+        'unread' => '',
+        'mute' => '',
+        'status' => '',
+        'type' => '',
+    ];
+
+    public function mount($img, $title, $excerpt, $time, $unread, $mute, $status, $type)
+    {
+        $this->model['img'] = $img;
+        $this->model['title'] = $title;
+        $this->model['excerpt'] = $excerpt;
+        $this->model['time'] = $time;
+        $this->model['unread'] = $unread;
+        $this->model['mute'] = $mute;
+        $this->model['status'] = $status;
+        $this->model['type'] = $type;
+    }
+
     public function render()
     {
         return view('livewire.card');
+    }
+
+    public function readMsg()
+    {
+        $this->model['status'] = true;
     }
 }
